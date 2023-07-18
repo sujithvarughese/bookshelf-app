8ceac85ad1cef512cb5a5552bbf8f3953f8d95b9
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-07-18 05:05:44 -0400

    created Book model so user can save books to preferred lists

---
+++ backend/models/Book.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+const BookSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: [true, 'Please provide book Title']
+	},
+	author: {
+		type: String,
+		required: [true, 'Please provide Author(s)']
+	},
+	coverID: {
+		type: String
+	},
+	genre: {
+		type: String
+	},
+	pages: {
+		type: Number
+	},
+	status: {
+		type: String,
+		enum: ['read', 'unread', 'reading']
+	},
+	rating: {
+		type: Number,
+		min: 1,
+		max: 10
+	},
+	notes: {
+		type: String
+	},
+	createdAt: {
+		type: Date,
+		default: () => Date.now(),
+		immutable: true
+	},
+	updatedAt: {
+		type: Date,
+		default: () => Date.now()
+	}
+}, { timestamps: true })
+
+export default mongoose.model('Book', BookSchema)
