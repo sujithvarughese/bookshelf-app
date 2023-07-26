12b38ee08bdf7ee10ab547710cab0374fd80c2bc
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-07-26 04:08:03 -0400

    added config in contants.js to select appropriate api based on environment

--- client/src/utils/ax.jsx
+++ client/src/utils/ax.jsx
@@ -1,34 +1,35 @@
 // axios
 import axios from "axios";
+import { config } from "../../constants.js";
 
 const axAPI = axios.create({
-  baseURL: "https://openlibrary.org",
-  withCredentials: true,
+	baseURL: "https://openlibrary.org",
+	withCredentials: true
 });
 // response
 axAPI.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    // console.log(error.response)
-    return Promise.reject(error);
-  }
+	(response) => {
+		return response;
+	},
+	(error) => {
+		// console.log(error.response)
+		return Promise.reject(error);
+	}
 );
 
 const axDB = axios.create({
-  baseURL: "http://localhost:8800/api/v1",
-  withCredentials: true,
+	baseURL: config.url.API_URL,
+	withCredentials: true
 });
 // response
 axDB.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    // console.log(error.response)
-    return Promise.reject(error);
-  }
+	(response) => {
+		return response;
+	},
+	(error) => {
+		// console.log(error.response)
+		return Promise.reject(error);
+	}
 );
 
 export { axAPI, axDB };
