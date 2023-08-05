e9fd009299f8dd3092a19789a99bf22a56ca4d90
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-08-05 15:04:56 -0400

    guest mode fix

--- client/src/components/GuestLogin.jsx
+++ client/src/components/GuestLogin.jsx
@@ -16,10 +16,8 @@ const GuestLogin = () => {
 		<div className="flex flex-col gap-6">
 			<div>Try Creating an account and get started!</div>
 			<div>
-				Or
-				<button className="underline text-blue-500 hover:cursor-pointer hover:text-cyan-500"
-				        onClick={guestLogin}>click here</button>
-				to access the site in Guest Mode!
+				Or <button className="underline text-blue-500 hover:cursor-pointer hover:text-cyan-500"
+				           onClick={guestLogin}>click here</button> to access the site in Guest Mode!
 			</div>
 		</div>
 	);
