73a9f749accec122ea89c46ce1dbbedc0422047a
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2023-08-05 15:25:39 -0400

    fixed bookshelf images not loading on deploy

--- client/src/assets/images/bookshelves/index.js
+++ client/src/assets/images/bookshelves/index.js
@@ -1,29 +1,53 @@
-import img1 from './AdobeStock_72396594.jpeg'
-import img2 from './AdobeStock_96515336.jpeg'
-import img3 from './AdobeStock_164752443.jpeg'
-import img4 from './AdobeStock_170365028.jpeg'
-import img5 from './AdobeStock_180469887.jpeg'
-import img6 from './AdobeStock_184711026.jpeg'
-import img7 from './AdobeStock_190212505.jpeg'
-import img8 from './AdobeStock_479026206.jpeg'
-import img9 from './AdobeStock_466091503.jpeg'
-import img10 from './AdobeStock_432981918.jpeg'
-import img11 from './AdobeStock_395963442.jpeg'
-import img12 from './AdobeStock_376877803.jpeg'
-import img13 from './AdobeStock_370157316.jpeg'
-import img14 from './AdobeStock_369251837.jpeg'
-import img15 from './AdobeStock_339418473.jpeg'
-import img16 from './AdobeStock_332772941.jpeg'
-import img17 from './AdobeStock_308975840.jpeg'
-import img18 from './AdobeStock_304090558.jpeg'
-import img19 from './AdobeStock_298722319.jpeg'
-import img20 from './AdobeStock_298028330.jpeg'
-import img21 from './AdobeStock_295366016.jpeg'
-import img22 from './AdobeStock_242107343.jpeg'
-import img23 from './AdobeStock_241739395.jpeg'
-import img24 from './AdobeStock_223259229.jpeg'
+import img1 from "./AdobeStock_72396594.jpeg";
+import img2 from "./AdobeStock_96515336.jpeg";
+import img3 from "./AdobeStock_164752443.jpeg";
+import img4 from "./AdobeStock_170365028.jpeg";
+import img5 from "./AdobeStock_180469887.jpeg";
+import img6 from "./AdobeStock_184711026.jpeg";
+import img7 from "./AdobeStock_190212505.jpeg";
+import img8 from "./AdobeStock_479026206.jpeg";
+import img9 from "./AdobeStock_466091503.jpeg";
+import img10 from "./AdobeStock_432981918.jpeg";
+import img11 from "./AdobeStock_395963442.jpeg";
+import img12 from "./AdobeStock_376877803.jpeg";
+import img13 from "./AdobeStock_370157316.jpeg";
+import img14 from "./AdobeStock_369251837.jpeg";
+import img15 from "./AdobeStock_339418473.jpeg";
+import img16 from "./AdobeStock_332772941.jpeg";
+import img17 from "./AdobeStock_308975840.jpeg";
+import img18 from "./AdobeStock_304090558.jpeg";
+import img19 from "./AdobeStock_298722319.jpeg";
+import img20 from "./AdobeStock_298028330.jpeg";
+import img21 from "./AdobeStock_295366016.jpeg";
+import img22 from "./AdobeStock_242107343.jpeg";
+import img23 from "./AdobeStock_241739395.jpeg";
+import img24 from "./AdobeStock_223259229.jpeg";
 
-const bookshelfImages = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24]
+export {
+	img1,
+	img2,
+	img3,
+	img4,
+	img5,
+	img6,
+	img7,
+	img8,
+	img9,
+	img10,
+	img11,
+	img12,
+	img13,
+	img14,
+	img15,
+	img16,
+	img17,
+	img18,
+	img19,
+	img20,
+	img21,
+	img22,
+	img23,
+	img24
+};
 
-export default bookshelfImages
 
--- client/src/components/forms/NewBookshelfForm.jsx
+++ client/src/components/forms/NewBookshelfForm.jsx
@@ -1,7 +1,34 @@
 import { useGlobalContext } from "../../context/GlobalContext.jsx";
 import { useState } from "react";
-import bookshelfImages from "../../assets/images/bookshelves/index.js";
+import {
+	img1,
+	img2,
+	img3,
+	img4,
+	img5,
+	img6,
+	img7,
+	img8,
+	img9,
+	img10,
+	img11,
+	img12,
+	img13,
+	img14,
+	img15,
+	img16,
+	img17,
+	img18,
+	img19,
+	img20,
+	img21,
+	img22,
+	img23,
+	img24
+} from "../../assets/images/bookshelves/index.js";
 import { Alert } from "..";
+
+const bookshelfImages = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24];
 
 const initialState = {
 	name: "",
